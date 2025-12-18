ac3ebdbce05cba95aabab056643b2a59df5fa85a
Jeff Mullins <jeffreydmullins0@gmail.com>
2025-12-18 00:48:34 -0600

    more commits for getting github pages working.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -196,7 +196,7 @@
       </div>
       <div class="panel-image">
       <a href="https://openalex.org/">
-        <img src=".\src\lib\assets\open_alex_logo.webp" alt="OpenAlex Logo"  width=200 />
+        <img src="./src/lib/assets/open_alex_logo.webp" alt="OpenAlex Logo"  width=200 />
       </a>
     </div>
     </div>  
@@ -299,8 +299,8 @@
       </p>  
     <div class="panel-body">
       <StackedArea
-        topicsFilePath="/topics_sampled.csv"
-        worksFilePath="/works_sampled.csv"
+        topicsFilePath="./topics_sampled.csv"
+        worksFilePath="./works_sampled.csv"
         width={1200}
         height={300}
       />
